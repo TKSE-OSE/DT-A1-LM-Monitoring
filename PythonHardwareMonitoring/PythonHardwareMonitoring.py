92c39408a5b34c6cc48214ea533bb5e4dc10225d
leon manitsch <leon.manitsch@gmail.com>
2021-01-25 14:37:32 +0100

    test

---
+++ PythonHardwareMonitoring/PythonHardwareMonitoring.py
@@ -0,0 +1,64 @@
+import shutil
+import psutil
+import os
+import mysql.connector
+from time import sleep
+from datetime import datetime
+                                       
+                                                                      
+while True:
+ total, used, free = shutil.disk_usage("/")
+
+ #Timestamp
+ print("="*40, "Timestamp", "="*39)                             #Header
+ now = datetime.now()
+ current_time = now.strftime("%H:%M:%S")
+ print("Current Timestamp =", current_time)
+
+ #HDD Space
+ totmem = "Total: %d GB" % (total // (2**30));
+ usedmem = "Used: %d GB" % (used // (2**30));
+
+ print("="*40, "Memory Info", "="*37)                           #Header
+ print(totmem)
+ print(usedmem)
+
+ #RAM
+ perram =psutil.virtual_memory().percent;
+ usedram = psutil.virtual_memory().used /1000000000;
+
+ print("="*40, "RAM Info", "="*40)                              #Header
+ print(perram, "%")                                             #Memory use in %
+ print(usedram, "GB")                                           #Memory use in GB
+
+ #CPU
+ cpuper = psutil.cpu_percent();
+ cpufreq = psutil.cpu_freq()
+
+ print("="*40, "CPU Info", "="*40)                              #Header
+ print(f"Current Frequency: {cpufreq.current:.2f}Mhz")          #Momentante Frequenz
+ print(cpuper, "%")
+
+ mydb = mysql.connector.connect(                                     
+  host="localhost",                                                 
+  user="root",                                                      
+  password="",                                                      
+  database="monitoringtask") 
+ mycursor = mydb.cursor()                                            
+                                               
+ sqltm = "INSERT INTO memory (id, computer_id, total, used) VALUES ('1','1',totmem,usedmem);"
+  
+ sqlpr = "INSERT INTO ram (id, computer_id, percentual, used) VALUES ('1','1',perram,usedram);"
+
+ sqlcf = "INSERT INTO cpu (id, computer_id, frequency, percent) VALUES ('1','1',cpufreq,cpuper);"
+               
+ # Insert into cpu (id, computerid, frequency, percent) VALUES ()
+
+ mycursor.execute(sqltm)
+ mycursor.execute(sqlpr)
+ mycursor.execute(sqlcf)
+ mydb.commit()                             
+                                             
+ print(mycursor.rowcount, "record inserted.") 
+
+ sleep(5)                                                       #5 second pause
